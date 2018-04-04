95ff1d6a0a7c1ceffef16a818490fd7eccf6dd84
Dương Văn Sơn <duongvanson@users.noreply.github.com>
2018-04-04 22:10:09 +0700

    Add quay so may man

---
+++ QuaySoMayMan/QuaySoMayMan/frmMain.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace QuaySoMayMan
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSo3 = new System.Windows.Forms.Label();
+            this.lblSo2 = new System.Windows.Forms.Label();
+            this.lblSo1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTienMay = new System.Windows.Forms.Label();
+            this.lblTienPlayer = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnQuay = new System.Windows.Forms.Button();
+            this.btnChoiLai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.timeRun = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSo3);
+            this.groupBox1.Controls.Add(this.lblSo2);
+            this.groupBox1.Controls.Add(this.lblSo1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 155);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblSo3
+            // 
+            this.lblSo3.BackColor = System.Drawing.Color.White;
+            this.lblSo3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSo3.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.lblSo3.ForeColor = System.Drawing.Color.Black;
+            this.lblSo3.Location = new System.Drawing.Point(291, 19);
+            this.lblSo3.Name = "lblSo3";
+            this.lblSo3.Size = new System.Drawing.Size(90, 130);
+            this.lblSo3.TabIndex = 0;
+            this.lblSo3.Text = "7";
+            this.lblSo3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSo2
+            // 
+            this.lblSo2.BackColor = System.Drawing.Color.White;
+            this.lblSo2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.lblSo2.ForeColor = System.Drawing.Color.Black;
+            this.lblSo2.Location = new System.Drawing.Point(145, 19);
+            this.lblSo2.Name = "lblSo2";
+            this.lblSo2.Size = new System.Drawing.Size(90, 130);
+            this.lblSo2.TabIndex = 0;
+            this.lblSo2.Text = "7";
+            this.lblSo2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSo1
+            // 
+            this.lblSo1.BackColor = System.Drawing.Color.White;
+            this.lblSo1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.lblSo1.ForeColor = System.Drawing.Color.Black;
+            this.lblSo1.Location = new System.Drawing.Point(12, 19);
+            this.lblSo1.Name = "lblSo1";
+            this.lblSo1.Size = new System.Drawing.Size(90, 130);
+            this.lblSo1.TabIndex = 0;
+            this.lblSo1.Text = "7";
+            this.lblSo1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTienMay);
+            this.groupBox2.Controls.Add(this.lblTienPlayer);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.ForeColor = System.Drawing.Color.Teal;
+            this.groupBox2.Location = new System.Drawing.Point(0, 155);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(389, 113);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin";
+            // 
+            // lblTienMay
+            // 
+            this.lblTienMay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTienMay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.lblTienMay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblTienMay.Location = new System.Drawing.Point(127, 79);
+            this.lblTienMay.Name = "lblTienMay";
+            this.lblTienMay.Size = new System.Drawing.Size(251, 30);
+            this.lblTienMay.TabIndex = 3;
+            this.lblTienMay.Text = "0";
+            this.lblTienMay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTienPlayer
+            // 
+            this.lblTienPlayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTienPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.lblTienPlayer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblTienPlayer.Location = new System.Drawing.Point(127, 32);
+            this.lblTienPlayer.Name = "lblTienPlayer";
+            this.lblTienPlayer.Size = new System.Drawing.Size(251, 30);
+            this.lblTienPlayer.TabIndex = 2;
+            this.lblTienPlayer.Text = "0";
+            this.lblTienPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tiền máy:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tiền người chơi:";
+            // 
+            // btnQuay
+            // 
+            this.btnQuay.BackColor = System.Drawing.Color.Teal;
+            this.btnQuay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnQuay.ForeColor = System.Drawing.Color.White;
+            this.btnQuay.Location = new System.Drawing.Point(15, 290);
+            this.btnQuay.Name = "btnQuay";
+            this.btnQuay.Size = new System.Drawing.Size(90, 30);
+            this.btnQuay.TabIndex = 2;
+            this.btnQuay.Text = "&Quay";
+            this.btnQuay.UseVisualStyleBackColor = false;
+            this.btnQuay.Click += new System.EventHandler(this.btnQuay_Click);
+            // 
+            // btnChoiLai
+            // 
+            this.btnChoiLai.BackColor = System.Drawing.Color.Teal;
+            this.btnChoiLai.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnChoiLai.ForeColor = System.Drawing.Color.White;
+            this.btnChoiLai.Location = new System.Drawing.Point(147, 290);
+            this.btnChoiLai.Name = "btnChoiLai";
+            this.btnChoiLai.Size = new System.Drawing.Size(90, 30);
+            this.btnChoiLai.TabIndex = 2;
+            this.btnChoiLai.Text = "&Chơi lại";
+            this.btnChoiLai.UseVisualStyleBackColor = false;
+            this.btnChoiLai.Click += new System.EventHandler(this.btnChoiLai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Teal;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnThoat.ForeColor = System.Drawing.Color.White;
+            this.btnThoat.Location = new System.Drawing.Point(279, 290);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(90, 30);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "&Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // timeRun
+            // 
+            this.timeRun.Interval = 10;
+            this.timeRun.Tick += new System.EventHandler(this.timeRun_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 341);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnChoiLai);
+            this.Controls.Add(this.btnQuay);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(405, 380);
+            this.MinimumSize = new System.Drawing.Size(405, 380);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quay Số May Mắn";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblSo3;
+        private System.Windows.Forms.Label lblSo2;
+        private System.Windows.Forms.Label lblSo1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblTienMay;
+        private System.Windows.Forms.Label lblTienPlayer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnQuay;
+        private System.Windows.Forms.Button btnChoiLai;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Timer timeRun;
+    }
+}
+
---
+++ QuaySoMayMan/QuaySoMayMan/frmMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuaySoMayMan
+{
+    public partial class frmMain : Form
+    {
+        int tienPlayer = 10000;
+        int tienMay = 10000;
+        int mot, hai, ba;
+        int count = 0;
+        Random rd = new Random();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        public void khoiTao()
+        {
+            lblTienMay.Text = tienMay + "";
+            lblTienPlayer.Text = tienPlayer + "";
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            khoiTao();
+        }
+
+        private void btnChoiLai_Click(object sender, EventArgs e)
+        {
+            tienMay = 10000;
+            tienPlayer = 10000;
+            khoiTao();
+            btnQuay.Enabled = true;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnQuay_Click(object sender, EventArgs e)
+        {
+            if (tienPlayer <= 0)
+            {
+                MessageBox.Show("Bạn hết tiền rồi!");
+                btnQuay.Enabled = false;
+            }
+            if (tienMay <= 0)
+            {
+                tienMay = 10000;
+            }
+            lblSo1.Text = mot + "";
+            lblSo2.Text = hai + "";
+            lblSo3.Text = ba + "";
+            if (mot != 7 && hai != 7 && ba != 7)
+            {
+                tienPlayer -= 1000;
+                tienMay += 1000;
+            }
+            if ((mot == 7 && hai != 7 && ba != 7) || (mot != 7 && hai == 7 && ba != 7) || (mot != 7 && hai != 7 && ba == 7))
+            {
+                tienPlayer += (int)(tienMay * 0.5);
+                tienMay -= (int)(tienMay * 0.5);
+            }
+            if ((mot == 7 && hai == 7 && ba != 7) || (mot != 7 && hai != 7 && ba == 7) || mot != 7 && hai == 7 && ba == 7)
+            {
+                tienPlayer += (int)(tienMay * 0.7);
+                tienMay -= (int)(tienMay * 0.7);
+            }
+            if (mot == 7 && hai == 7 && ba == 7)
+            {
+                MessageBox.Show("THẮNG LỚN!");
+                tienPlayer += tienPlayer;
+                tienMay -= tienPlayer;
+            }
+            khoiTao();
+            timeRun.Start();
+        }
+        private void timeRun_Tick(object sender, EventArgs e)
+        {
+            mot = rd.Next(0, 7);
+            hai = rd.Next(0, 8);
+            ba = rd.Next(0, 9);
+            lblSo1.Text = mot + "";
+            lblSo2.Text = hai + "";
+            lblSo3.Text = ba + "";
+            if (count > 100)
+            {
+                count = 0;
+                timeRun.Stop();
+            }
+            count++;
+        }
+    }
+}
